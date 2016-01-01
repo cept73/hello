e42944fdd0d1cf43acf3373d9ac0e6eea057ca6c
Sergey Taranov <cept73@yandex.ru>
2015-12-31 23:51:49 -0500

    first commit

---
+++ views/site/about.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Втука?';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>wer</p>
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+<?php echo 'lo hi' ?>
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
